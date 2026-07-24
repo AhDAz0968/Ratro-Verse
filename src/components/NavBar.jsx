4cf944be6e73c461a59096844910137dfcadc8d5
Oa Kakada <xxkakadaxx123@gmail.com>
2026-07-24 17:43:26 +0700

    add hamburger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './NavBar.css';
 import logo from '../assets/logo-dark-transparent.png'
 
 export default function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <Link to="/" className="nav-logo">
         <img src={logo} alt="RetroGame logo" className='logo-img'/>
       </Link>
+
+      <button 
+        className="hamburger" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
       
-      <div className="nav-links">
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/about" className="nav-link">About</Link>
         <Link to="/cervice" className="nav-link">Cervice</Link>
         <Link to="/contact" className="nav-link">Contact</Link>
         <Link to="/login" className="login-btn">Login</Link>
-
       </div>
+
     </nav>
   );
 }
