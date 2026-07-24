ef2b1becb8add1870cd3b156c2474af3e2c24798
Oa Kakada <xxkakadaxx123@gmail.com>
2026-07-24 23:51:14 +0700

    add a bit of content to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
+import './Home.css';
+import crtTV from '../assets/CRT-TV.jpg'
+
+
 export default function Home() {
   return (
-    <div style={{ padding: '3rem 2rem', textAlign: 'center' }}>
-      <h1 style={{ fontSize: '2.5rem', marginBottom: '1rem' }}>
-        🕹️ Welcome to Retro Game
-      </h1>
-      <p style={{ color: '#aaa', fontSize: '1.1rem' }}>
-        Your classic arcade experience awaits. Click <strong>Login</strong> in the navbar to get started!
-      </p>
+    <div className="heroHeader-container">
+
+      <div className="heroHeader">
+        <h3>█████ RETRO GAME HUB █████</h3>
+        <p>Discover Classic Games From The Golden Era Of Gaming</p>
+      </div>
+      <div className="headerDescription-container">
+        
+        <div className="headerDiscription">
+           <p>Browse legendary titles, explore hidden gems, and find your next retro adventure.</p>
+
+          <button className='headerDesc-btn'>Explore Games</button>
+          <button className='headerDesc-btn'>Join Community</button>
+        </div>
+
+        <img src={crtTV} alt="CRT TV" className='desc-img'/>
+
+      </div>
     </div>
   );
 }
