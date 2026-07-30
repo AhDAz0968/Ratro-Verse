5d44d35a950bd3550ac816c8b3dc3638500cacad
Oa Kakada <xxkakadaxx123@gmail.com>
2026-07-30 16:19:36 +0700

    add theme changing to About Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
 import './Footer.css'
 
+import { useLocation } from 'react-router-dom';
+
 export default function Footer(){
+    //change theme
+  const location = useLocation();
+  
+    const themes = {
+    "/": "retro",
+    "/about": "gameBoy",
+    "/games": "arcade",
+    "/login": "dark",
+  };
+
+  const theme = themes[location.pathname] || "retro";
+
     return(
-        <footer className="footer-container">
+        <footer className={`footer-container ${theme}`}>
             <div className="footerItems">
                 <h5>RetroVerse</h5>
                 <p>Discover the best classic games from the past.</p>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,27 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './NavBar.css';
-import logo from '../assets/logo-dark-transparent.png'
+import logo from '../assets/logo-dark-transparent.png';
+import { useLocation } from 'react-router-dom';
 
 export default function Navbar() {
-
+  //toggle hambuger
   const [menuOpen, setMenuOpen] = useState(false);
 
+  //change theme
+  const location = useLocation();
+  
+    const themes = {
+    "/": "retro",
+    "/about": "gameBoy",
+    "/games": "arcade",
+    "/login": "dark",
+  };
+
+  const theme = themes[location.pathname] || "retro";
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${theme}`}>
       <Link to="/" className="nav-logo">
         <img src={logo} alt="RetroGame logo" className='logo-img'/>
       </Link>
@@ -20,10 +33,10 @@ export default function Navbar() {
         ☰
       </button>
       
-      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+      <div className={`nav-links ${menuOpen ? "active" : ""} `}>
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/about" className="nav-link">About</Link>
-        <Link to="/cervice" className="nav-link">Service</Link>
+        <Link to="/service" className="nav-link">Service</Link>
         <Link to="/contact" className="nav-link">Contact</Link>
         <Link to="/login" className="login-btn">Login</Link>
       </div>
