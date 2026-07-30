4b2b371b80b425c7421c2eb5a416340f5523b56a
Oa Kakada <xxkakadaxx123@gmail.com>
2026-07-31 00:27:42 +0700

    add more content to About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ import gamingHistory from '../assets/gamingHistory.png'
 import discoverGames from '../assets/discoverGames.png'
 import gamingCommunity from '../assets/gamingCommunity.png'
 
+import ninjaWarrior from '../assets/ninja-warrior.png'
+
 
 function About() {
   return (
@@ -58,9 +60,100 @@ function About() {
               <p>Build a Retro Community</p>
             </div>
           </div>
-          
-          
+        </div>
+      </div>
 
+      <div className="projectObj-container">
+
+            <div className="projectObj">
+              <h3>PROJECT OBJECTIVES</h3>
+            </div>
+
+            <ol className='projectObjLists'>
+              <li>Create a web platform for retro game information.</li>
+              <li>Recommend classic games to users.</li>
+              <li>Implement secure user authentication.</li>
+              <li>Provide an admin dashboard for content management.</li>
+              <li>Manage game records using CRUD operations.</li>
+            </ol>
+
+      </div>
+
+      <div className="whatWeOffer-container">
+
+        <div className="whatWeOffer">
+          <h3>WHAT WE OFFER</h3>
+        </div>
+
+        <div className="whatWeOfferItems">
+          <div className="border-01">
+            <div className="whatWeOfferItem">
+              <img src={gamingHistory} alt="gaming history" className='whatWeOfferIcon'/>
+              <h4>Game Database</h4>
+              <p>Browse retro games</p>
+            </div>
+          </div>
+          <div className="border-01">
+            <div className="whatWeOfferItem">
+              <img src={gamingHistory} alt="Discover Games" className='whatWeOfferIcon'/>
+              <h4>Recommendations</h4>
+              <p>Discover hidden gems</p>
+            </div>
+          </div>
+          <div className="border-01">
+            <div className="whatWeOfferItem">
+              <img src={gamingHistory} alt="gaming history" className='whatWeOfferIcon'/>
+              <h4>Community Reviews</h4>
+              <p>Read player opinions</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="platformFeature-container">
+
+        <div className="platformFeature">
+          <h3>PLATFORM FEATURES</h3>
+        </div>
+        <div className="outside-border">
+          <div className="platformFeatureContent">
+
+              <p>
+                ✓ User Registration <br /> 
+                                                                            
+                ✓ Login & Authentication <br />      
+
+                ✓ Game Search <br /> 
+
+                ✓ Game Recommendations <br />
+
+                ✓ Admin Dashboard <br />
+
+                ✓ CRUD Management <br />
+
+                ✓ Responsive Design <br />
+              </p>
+
+          </div>
+        </div>
+        
+      </div>
+
+      <div className="meetTheDev-container">
+
+        <div className="DevInfoContainer">
+            <div className="meetTheDev">
+              <h3>MEET THE DEVELOPER</h3>
+            </div>
+            <div className="DeveloperInfo">
+              <img src={ninjaWarrior} alt="me :)" className='myImg'/>
+              <h4 className='myName'>Oa Kakada</h4>
+              <p className='currentJob'>Student / Web Developer</p>
+              <p>
+                Developed using React.js and Supabase to create a modern platform for retro game enthusiasts.
+              </p>
+            </div>
         </div>
         
       </div>
